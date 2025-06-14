7f0f06bca3ecd24732b55b97d0d462a0c49419c9
Aariya890 <Shrabana16@gmail.com>
2025-06-14 22:23:29 +0600

    Add files via upload

---
+++ 1017.triome.py
@@ -0,0 +1,27 @@
+import math
+
+print(math.log10(3.14159))
+print(math.log10(5))
+print(math.log10(6))
+print(f"\n")
+
+print(math.exp(98))
+print(math.exp(-5.13))
+print(f"\n")
+
+print(math.isnan(12))
+print(math.isnan(-78.9))
+print(math.isnan(math.nan))
+print(f"\n")
+
+print(math.sqrt(625))
+print(math.sqrt(22))
+print(math.sqrt(16))
+print(f"\n")
+
+print(math.sin(0.00))
+print(math.sin(-71.23))
+
+
+
+
