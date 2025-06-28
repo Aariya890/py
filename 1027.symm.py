9f1c9a7db0289cb8e952c29fc7a210bbf5d9739f
Aariya890 <Shrabana16@gmail.com>
2025-06-28 22:06:12 +0600

    Add files via upload

---
+++ 1027.symm.py
@@ -0,0 +1,6 @@
+set_a = input("Enter the first set: ")
+set_b = input("Enter the second set: ")
+
+symm_diff = set_a.symmetric_difference(set_b)
+
+print("Their symmentric difference is: ", symm_diff)
