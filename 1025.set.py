46a3fec62c6c8c3739df632624fc3fd9728ed4c6
Aariya890 <Shrabana16@gmail.com>
2025-06-28 21:26:45 +0600

    Add files via upload

---
+++ 1025.set.py
@@ -0,0 +1,40 @@
+def display_menu():
+    print("\nSet Operations Menu ")
+    print("1. Union")
+    print("2. Intersection")
+    print("3. Difference (A - B)")
+    print("4. Difference (B - A)")
+    print("5. Symmetric  difference")
+    print("6. Exit")
+
+def main():
+    print("Welcome to the Set Operations Project!")  
+    set_a = {'cherry', 'durian','apple'}
+    set_b = {'cherry', 'watermelon', 'orange'}
+    
+    print("Set A: ", set_a)
+    print("Set B: ", set_b)
+    
+    while True:
+        display_menu()
+        choice = input("Choose an operation(1-6):  ")
+        
+        if choice == '1':
+            print(set_a.union(set_b))
+        elif choice == '2':
+            print(set_a.intersection(set_b))    
+        elif choice == '3':
+            print(set_a.difference(set_b)) 
+        elif choice == '4':
+            print(set_b.difference(set_a))  
+        elif choice == '5':
+            print(set_a.symmetric_difference(set_b)) 
+        elif choice == '6':
+            print("Exiting. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please enter from 1 to 6.")
+
+if __name__ == "__main__":
+    main()           
+                                       
