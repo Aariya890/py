097f67f868519d38273fcfc747381184091a7232
Aariya890 <Shrabana16@gmail.com>
2025-07-22 22:15:14 +0600

    Add files via upload

---
+++ 1042.string.py
@@ -0,0 +1,11 @@
+def __init__(self, text):
+    self.text = text
+
+def main(self):
+    user_input = input("Enter a string: ")
+    s = user_input
+    reverse =  self.text[::-1]
+    print("Reversed:", reverse())   
+
+if __name__ == "__main__" :
+    main()
