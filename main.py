c62ddf3fab6082e56de4984a71404c0a820da7c0
Aariya890 <Shrabana16@gmail.com>
2025-07-29 21:28:45 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-from abc import ABC, abstractmethod
+import random
 
-class Absclass(ABC):
+class Flashcard():
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+        
+class FlashcardDeck:
+    def __init__(self):
+        self.cards = []
+    def add_card(self, question, answer) :
+        self.cards.append(Flashcard(question, answer))
+    def shuffle(self):
+        random.shuffle(self.cards)     
+    def quiz(self):
+        score = 0
+        for card in self.cards:
+            print(f"Question: {card.question}")
+            user_answer = input("Enter your answer: ")
+            if user_answer.strip().lower() == card.answer.strip().lower():
+                print("Correct!\n")
+                score += 1
+            else:
+                print(f"Wrong! The answer is {card.answer}\n.")    
+        print(f"Quiz finished! Your score: {score}/{len(self.cards)}")        
     
-    def print(self, x):
-        print("Passed value:", x)
-        
-    @abstractmethod
-    def task(self):
-        print("We are inside Absclass task.")
-        
-class test_class(Absclass):
-    def task(self):
-        print("We are inside task_class task.")
-        
-test_obj = test_class()                
-test_obj.task()
-test_obj.print(100)       
+def main():
+    deck = FlashcardDeck()
+    print("Enter your flashcards(or 'done' to finish): ")  
+    while True:
+        q = input("Question: ") 
+        if q.lower() == 'done' :
+            break
+        a = input("Answer: ")
+        deck.add_card(q, a)
+    deck.shuffle()    
+    deck.quiz()
+
+if __name__ == "__main__":
+    main()    
